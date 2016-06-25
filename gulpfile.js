0efc4d3971306f42d4ca7a197dbf1a440c25eea1
yonikan <yonikan@gmail.com>
2016-06-25 18:45:19 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,78 @@
+//
+// var gulp = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var concat = require('gulp-concat');
+// var sass = require('gulp-sass');
+//
+// // Static Server + watching scss/html files
+// gulp.task('serve', ['sass'], function() {
+//
+//     browserSync.init({
+//         server: "./"
+//     });
+//
+//     // Watch .js files
+//     gulp.watch('./js/*.js', ['scripts']);
+//      // Watch .scss files
+//     gulp.watch('./scss/*.scss', ['sass']);
+//
+//     gulp.watch("*.html").on('change', browserSync.reload);
+// });
+//
+//
+// // Concatenate JS Files
+// gulp.task('scripts', function() {
+//    return gulp.src('./js/*.js')
+//      .pipe(concat('main.js'))
+//      .pipe(gulp.dest('bundle'))
+//      .pipe(browserSync.stream());
+// });
+//
+//
+// // Compile sass into CSS & auto-inject into browsers
+// gulp.task('sass', function() {
+//     return gulp.src("./scss/**/*.scss")
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest("bundle"))
+//         .pipe(browserSync.stream());
+// });
+//
+//
+// gulp.task('default', ['serve']);
+//
 
+
+
+
+
+// ------ whithout sync -------------------------------------------------------------
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-
-
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    // Watch .js files
-    gulp.watch('./js/*.js', ['scripts']);
-     // Watch .scss files
-    gulp.watch('./scss/*.scss', ['sass']);
-
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
 
 
 // Concatenate JS Files
 gulp.task('scripts', function() {
    return gulp.src('./js/*.js')
      .pipe(concat('main.js'))
-     .pipe(gulp.dest('bundle'))
-     .pipe(browserSync.stream());
+     .pipe(gulp.dest('bundle'));
 });
 
 
-// Compile sass into CSS & auto-inject into browsers
+// Compile sass into CSS
 gulp.task('sass', function() {
     return gulp.src("./scss/**/*.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("bundle"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("bundle"));
 });
 
 
-gulp.task('default', ['serve']);
+gulp.task('watch', function() {
+   // Watch .js files
+   gulp.watch('./js/*.js', ['scripts']);
+    // Watch .scss files
+   gulp.watch('./scss/*.scss', ['sass']);
+ });
+
+ // Default Task
+ gulp.task('default', ['scripts', 'sass', 'watch']);
